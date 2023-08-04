9105a1e7ace780d4a4175920dbb2c9ebdaf58dda
Shrikant Rajendra Patil <58933004+shrikant2753@users.noreply.github.com>
2023-08-04 18:05:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 function getFormvalue() {
     //Write your code here
 
+	let form = document.getElementById("form1")
+	let firstName = form.fname.value
+	let lastName = form.lname.value
+
+	alert(firstName + " "+ lastName)
+
 }
